56095d6ca1170e88c8d79c55d56c79809581d70d
Tri Phan <phanphutri@gmail.com>
2023-05-08 07:47:33 +0700

    Updated

--- chapter_08_lists/sample.py
+++ chapter_08_lists/sample.py
@@ -131,29 +131,160 @@ Traversing a list
 # print(cars)
 
 """Sort a list of dictionaries based on the "year" value of the dictionaries"""
-# item = {'car': 'Ford', 'year': 2005}
-# print(type(item['year']))
-cars = [
-    {'car': 'Ford', 'year': 2005},
-    {'car': 'Mitsubishi', 'year': 2000},
-    {'car': 'BMW', 'year': 2019},
-    {'car': 'VW', 'year': 2011},
-    {'car': 'ABCDEF'},
-]
-
-
-def get_year(e):
-    return e['year']
-
-
-cars.sort(key=get_year)
-print(cars)
-# [
-#     {'car': 'Mitsubishi', 'year': 2000},
+# cars = [
 #     {'car': 'Ford', 'year': 2005},
+#     {'car': 'Mitsubishi', 'year': 2000},
+#     {'car': 'BMW', 'year': 2019},
 #     {'car': 'VW', 'year': 2011},
-#     {'car': 'BMW', 'year': 2019}
+#     {'car': 'ABCDEF'},
 # ]
+#
+#
+# def get_year(e):
+#     return e['year']
+#
+#
+# cars.sort(key=get_year)
+# print(cars)
+# # [
+# #     {'car': 'Mitsubishi', 'year': 2000},
+# #     {'car': 'Ford', 'year': 2005},
+# #     {'car': 'VW', 'year': 2011},
+# #     {'car': 'BMW', 'year': 2019}
+# # ]
+
+"""Deleting elements"""
+
+"""
+If you know the index of the element you want, you can use pop
+If you don't provide an index, it deletes and returns the last element.
+"""
+# t = ['a', 'b', 'c']
+
+# x = t.pop(1)
+# print(t)  # ['a', 'c']
+# print(x)  # b
+
+# y = t.pop()
+# print(t)  # ['a', 'b']
+# print(y)  # c
+
+"""
+If you know the element you want to remove (but not the index), you can use remove.
+The return value from remove is None.
+"""
+# t = ['a', 'b', 'c']
+
+# t.remove('b')
+# print(t)  # ['a', 'c']
+
+# t.remove('d')  # ValueError: list.remove(x): x not in list
+
+# if 'd' in t:
+#     t.remove('d')
+
+"""To remove more than one element, you can use del with a slice index"""
+# t = ['a', 'b', 'c', 'd', 'e', 'f']
+# del t[2:5]
+# print(t)  # ['a', 'b', 'f']
+
+"""
+There are a number of built-in functions that can be used on lists that allow you
+to quickly look through a list without writing your own loop
+
+The sum() function only works when the list elements are numbers. The other
+functions (max(), len(), etc.) work with lists of strings and other types that can
+be comparable.
+"""
+
+# nums = [3, 41, 12, 9, 74, 15]
+# print(len(nums))  # 6
+# print(max(nums))  # 74
+# print(min(nums))  # 3
+# print(sum(nums))  # 154
+# print(sum(nums) / len(nums))  # 25.666666666666668
+# print(round(sum(nums) / len(nums), 2))  # 25.67
+
+
+# def compute_average():
+#     total = 0
+#     count = 0
+#     print('Type "done" if you want to finish entering number')
+#     while True:
+#         inp = input('Enter a number: ')
+#         if inp == 'done':
+#             break
+#         try:
+#             inp = float(inp)
+#         except ValueError:
+#             print('Incorrect format. Please enter a number!')
+#             continue
+#         total += inp
+#         count += 1
+#     if count != 0:
+#         return total / count
+#     return None
+
+def compute_average():
+    print('Type "done" if you want to finish entering number')
+    t = []
+    while True:
+        inp = input('Enter a number: ')
+        if inp == 'done':
+            break
+        try:
+            inp = float(inp)
+        except ValueError:
+            print('Wrong format. Please enter a number!')
+            continue
+        t.append(inp)
+    if len(t) == 0:
+        return None
+    return sum(t)/len(t)
+
+
+result = compute_average()
+if result is None:
+    print('Can not compute average')
+else:
+    print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
